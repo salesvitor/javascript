97c7c1377ff8493672398109d0d1042b13a78648
Vitor Sales <vitorluzdesales@gmail.com>
2023-11-21 21:47:48 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function calcular() {
+    var peso = window.document.getElementById('peso')
+    var altura = window.document.getElementById('altura')
+    var res = window.document.getElementById('res')
+    var n1 = Number(peso.value)
+    var n2 = Number(altura.value)
+    var s1 = n1 / (n2 ** 2)
+    res.innerHTML = `Resultado: Seu IMC é ${s1.toFixed(1)}`
+ }
